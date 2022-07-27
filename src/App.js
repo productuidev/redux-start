1791e086c8a00b576160d4e5b02fd32a31b8a629
productuidev <productuidev@gmail.com>
2022-07-27 19:33:59 +0900

    create common custom hooks

--- src/App.js
+++ src/App.js
@@ -3,27 +3,9 @@ import './App.css';
 import { useState, useEffect, useContext } from 'react';
 import { addTodo } from './redux/actions';
 import ReduxContext from './contexts/ReduxContext';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
-function useReduxState() {
-  const store = useContext(ReduxContext);
-  const [state, setState] = useState(store.getState()); // 초기값
-
-  useEffect(()=>{
-    const unsubscribe = store.subscribe(()=>{
-      setState(store.getState());
-    });
-
-    return () => {
-      unsubscribe();
-    };
-  }, [store]); // dependency
-
-  // store가 들어올 때, 다른 store면 다시 실행하는 것이므로 검토해보면
-  // index.js에서 다른 store를 다시 넣어주는 경우는 없음
-  // 결과적으로 1번만 실행
-
-  return state;
-}
 
 // 공통 로직
 // dispatch를 반환하면 그 결과물을 useReduxDispatch로 가져올 수 있음
@@ -46,6 +28,8 @@ function App() {
         {JSON.stringify(state)} {/* 객체를 JSON 문자열로 변환*/}
         <button onClick={click}>추가</button>
       </header>
+      <TodoList />
+      <TodoForm />
     </div>
   );
 
