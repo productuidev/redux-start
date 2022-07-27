775fc18752693799661135011e85e871798a266a
productuidev <productuidev@gmail.com>
2022-07-28 06:53:43 +0900

    Presentational Componenct and Connect Container

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,7 +1,11 @@
 import { useRef } from "react";
+import { connect } from 'react-redux';
+import { addTodo } from "../redux/actions";
 
-// uncontrolled components
-export default function TodoForm() {
+// 위쪽으로 이동
+// TodoForm은 함수를 받아서 보여주는 역할(Presentational Component)
+// Container가 주는 data를 받아서 그냥 보여주거나 실행함
+function TodoForm({ add }) {
   const inputRef = useRef();
 
   return (
@@ -13,6 +17,20 @@ export default function TodoForm() {
   // 버튼 클릭 시 addTodo 액션
   // dispatch
   function click() {
-    // dispatch(addTodo(inputRef.current.value));
+    // 직접 dispatch하지 않고 add를 props으로 받아서 실행
+    add(inputRef.current.value);
   }
 }
+
+// Container = Smart Component로 역할 분리
+// Store와 Presentatinal Component를 이어주는 역할 = connect
+export default connect(
+  (state) => ({}),
+
+  // todos를 실행하는 로직
+  (dispatch) => ({
+    add: (text) => {
+      dispatch(addTodo(text));
+    }
+  })
+)(TodoForm);
